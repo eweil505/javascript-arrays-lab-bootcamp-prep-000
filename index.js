9b227988732e6262b7a41f2413bc93464b511e23
Erika Weil <erikaweil@outlook.com>
2017-08-30 15:36:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 var kittens = ["Milo", "Otis", "Garfield"];
 
-function destructivelyAppendKitten(kittens) {
-  kittens.push("Betty");
-  return kittens; 
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  var newArr = kittens.push(name);
+  return newArr; 
 }
